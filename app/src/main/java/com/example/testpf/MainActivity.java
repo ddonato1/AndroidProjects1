8ffc88a3027beba62df7f110cb5a57ac26d79f66
ddonato1 <ddonato1@email.suagm.edu>
2019-06-03 09:02:05 -0400

    part2 activity

--- app/src/main/java/com/example/testpf/MainActivity.java
+++ app/src/main/java/com/example/testpf/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.testpf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText busNum;
+    EditText volNum;
+    EditText jacNum;
+    private Button btnMove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         busNum = findViewById(R.id.BusesQ);
+        volNum = findViewById(R.id.Voltage);
+
+        btnMove = findViewById(R.id.buttonPart2);
+        btnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveButton();
+            }
+        });
+
     }
 
     public void saveButton(View view){
@@ -22,5 +37,10 @@ public class MainActivity extends AppCompatActivity {
         int busNUMF = Integer.parseInt(buses);
 
         Toast.makeText(this, "Answer has been saved " +busNUMF, Toast.LENGTH_SHORT).show();
+    }
+
+    private void moveButton(){
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
     }
 }
